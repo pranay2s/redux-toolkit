67822ac6fbe946f48796aa9b46a5866c686a5bb3
pranay2s <99501441+pranay2s@users.noreply.github.com>
2025-02-20 08:33:04 +0000

    created react redux tookit setup

---
+++ redux-toolkit-setup/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import UserForm from './pages/user/user';
+import UserList from './pages/user/user_list';
+
+function App() {
+  return (
+    <>
+    <UserForm />
+    <UserList/>
+    </>
+  );
+}
+
+export default App;
